bb5dc6ed9fef296b0238037d75430190071504a6
KA-rg <krishjain260905@gmail.com>
2026-01-11 22:03:38 +0530

    Open account section made

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 function OpenAccount() {
   return (
-    <div>
-      <h1>Open an Account</h1>
-      <p>Please fill out the form below to open a new account.</p>
+    <div className='container p-5'>
+      <div className='row align-items-center justify-content-center'>
+        <div className='col-md-6'>
+          <h1 className='display-4 fw-bold'>Open a Kredit Account</h1>
+          <p className='lead'>Modern platform and app with zero investment, and flat 20$ off on intraday.</p>
+          <a href='/signup' className='btn btn-primary btn-lg'>Sign Up Now</a>
+        </div>
+      </div>
     </div>
   );
 }
